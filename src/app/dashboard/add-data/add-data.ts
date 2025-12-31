1072f7517639bc91e575bdc339678a9ef9007901
Mais <mayrmichael@gmx.net>
2025-12-31 16:37:18 +0100

    feat: add angular material & css grid WIP

--- src/app/dashboard/add-data/add-data.ts
+++ src/app/dashboard/add-data/add-data.ts
@@ -1,11 +1,26 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroupDirective, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Store } from '../../shared/store';
 import { Backend } from '../../shared/backend';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from "@angular/material/button";
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-data',
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './add-data.html',
   styleUrl: './add-data.scss',
 })
@@ -14,18 +29,35 @@ export class AddData {
   public backend = inject(Backend);
   private fb = inject(FormBuilder);
   public signupForm: any;
+  @ViewChild(FormGroupDirective)
+  private formDirective!: FormGroupDirective;
 
   ngOnInit() {
     this.signupForm = this.fb.group({
       name: ['', Validators.required],
       birthdate: ['', Validators.required],
       courseId: ['', Validators.required],
+      newsletter: [false]
     });
   }
 
   onSubmit() {
     if (this.signupForm.valid) {
-      this.backend.addRegistration(this.signupForm.value);
+      const formValue = this.signupForm.value;
+
+      const requestBody = {
+        ...formValue,
+        birthdate: formValue.birthdate.toISOString().split('T')[0],
+      }
+
+      this.backend.addRegistration(requestBody);
+      
+      this.formDirective.resetForm({
+        name: '',
+        birthdate: null,
+        courseId: '',
+        newsletter: false,
+      });
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 bootstrapApplication(App, {
-  providers: [provideHttpClient(), provideRouter(routes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideHttpClient(), 
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+  provideNativeDateAdapter()],
 });
